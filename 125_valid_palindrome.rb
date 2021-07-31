87954d3aa6ffb987c24a564429b33a4a219031d1
shaundreamsincode <77544022+shaundreamsincode@users.noreply.github.com>
2021-07-31 16:04:27 -0400

    Create 125_valid_palindrome.rb

---
+++ 125_valid_palindrome.rb
@@ -0,0 +1,19 @@
+# @param {String} s
+# @return {Boolean}
+def is_palindrome(s)
+    s.downcase!
+    
+    i = s[0]
+    j = s[s.length-1]
+    
+    while i < j do
+        if s[i] != s[j]
+            return false
+        else
+            i += 1
+            j -= 1
+        end
+    end
+    
+    true
+end
