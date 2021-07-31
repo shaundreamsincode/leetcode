4bea86adb436400abf2ef2840998ca8275da8527
shaundreamsincode <77544022+shaundreamsincode@users.noreply.github.com>
2021-07-31 15:25:45 -0400

    Create two_sum.rb

---
+++ two_sum.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    hash = {}
+        
+    nums.each_with_index do |num, index|
+        diff = target - num
+        
+        if hash[diff]
+            return [index, hash[diff]]
+        else
+            hash[num] = index
+        end
+    end    
+end
